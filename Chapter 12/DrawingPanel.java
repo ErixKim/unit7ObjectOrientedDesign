9a9066f606216659cc01500bf616bb80e4e94d09
ErixKim <edkim@stu.naperville203.org>
2016-03-03 23:21:20 -0600

    3/3/2016

--- Chapter 12/DrawingPanel.java
+++ Chapter 12/DrawingPanel.java
@@ -5,6 +5,8 @@ import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ import java.util.Random;
 import javax.swing.JColorChooser;
 import java.awt.geom.Point2D;
 import java.awt.Graphics2D;
+import java.awt.event.MouseMotionListener;
 public class DrawingPanel extends JPanel
 {
     private ArrayList<Shape> shapeList = new ArrayList<Shape>();
@@ -23,7 +26,6 @@ public class DrawingPanel extends JPanel
     {
         this.setBackground(Color.WHITE);
         activeColor = Color.BLUE;
-        //         MouseListener listener = new MousePressListener();
     }
 
     public Color getColor()
@@ -44,53 +46,65 @@ public class DrawingPanel extends JPanel
 
     public void addCircle()
     {
-        Circle circle = new Circle(this.activeColor,new Point2D.Double(100,100),100);
-        shape.add(circle);
+        //         Circle circle = new Circle(new Point2D.Double(100,100),100,this.activeColor);
+        //         Shape.addcircle;
     }
 
     public void addSquare()
     {
-        Circle square = new Circle(this.activeColor,new Point2D.Double(100,100),100);
-        shape.add(square);
+        //         Circle square = new Circle(new Point2D.Double(100,100),100,this.activeColor);
+        //         Shape.add(square);
     }
 
     public void paintComponent(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
         super.paintComponent(g);
-        for(Shape shapes: this.shape)
+        for(Shape shapeList: this.shapeList)
         {
-            shapes.draw(g2,true);
+            shapeList.draw(g2,true);
         }
         repaint();
     }
-    //     public class MyMouseListener implements MouseListener
-    //     {
-    //         public void mousePressed(MouseEvent event)
-    //         {
-    //             int x = event.getX();
-    //             int y = event.getY();
-    //         }
-    // 
-    //         public void mouseReleased(MouseEvent event)
-    //         {
-    // 
-    //         }
-    // 
-    //         public void mouseClicked(MouseEvent event) 
-    //         {
-    // 
-    //         }
-    // 
-    //         public void mouseEntered(MouseEvent event) 
-    //         {
-    // 
-    //         }
-    // 
-    //         public void mouseExited(MouseEvent event)
-    //         {
-    // 
-    //         }
-    //     }
-    //     
+    public class MyMouseListener implements MouseListener
+    {
+        public void mousePressed(MouseEvent event)
+        {
+            int x = event.getX();
+            int y = event.getY();
+        }
+
+        public void mouseReleased(MouseEvent event)
+        {
+
+        }
+
+        public void mouseClicked(MouseEvent event) 
+        {
+
+        }
+
+        public void mouseEntered(MouseEvent event) 
+        {
+
+        }
+
+        public void mouseExited(MouseEvent event)
+        {
+
+        }
+    }
+    
+    public class MyMouseMotionListener implements MouseMotionListener
+    {
+        public void mouseDragged(MouseEvent event)
+        {
+            
+        }
+        
+        public void mouseMoved(MouseEvent event)
+        {
+            
+        }
+    }
 }
--- Chapter 12/Square.java
+++ Chapter 12/Square.java
@@ -1,4 +1,10 @@
+import java.awt.geom.Point2D;
+import java.awt.Color;
+import java.awt.Graphics2D;
 public class Square extends Shape
 {
-    
+    public Square(Point2D.Double center, double radius, Color color)
+    {
+        super(center, radius, color);
+    }
 }
